10b658bd557cf43e8c88881a3e31b6440631cbf9
Antti Leppä <antti.leppa@metatavu.fi>
2019-08-21 17:44:39 +0300

    Added better examples for locations and images

--- linkedevents/event.php
+++ linkedevents/event.php
@@ -1,18 +1,34 @@
 <?php
-  $result = "";
 
+  $result = "";
   $result .= '<article>';
   $result .= '<div>';
-  
-  $eventName = $event["name"]["fi"];
+
+  $eventLocation = $event["location"];
+  $eventName = getLocalizedValue($event["name"], "fi");
   $eventLink = $event["externalLinks"][0]["link"];
-  $eventDescription = $event["description"]["fi"];
+  $eventDescription = getLocalizedValue($event["description"], "fi");
   $eventTime = $event["startTime"]->format("l jS F Y G:ia");
+  $location = $data->locations[$eventLocation["id"]];
+
+  $result .= '<div style="float: right">';
+  foreach ($event["images"] as $image) {
+    $result .= sprintf('<img style="max-width: 200px; margin-right: 5px;" src="%s"/>', $image["url"]);
+  }
+  $result .= '</div>';
 
   $result .= sprintf('<a href="%s">%s</a>', $eventLink, $eventName);
+  $result .= '<div>';
   $result .= sprintf('<p>%s</p>', $eventTime);
-  $result .= sprintf('<p style="color: red">%s</p>', html_entity_decode($eventDescription));
   
+  if ($location) {
+    $address = formatLocationAddress($location, "fi");
+    $result .= $address;
+  }
+
+  $result .= sprintf('<p style="font-style: italic">%s</p>', nl2br(html_entity_decode($eventDescription)));
+  $result .= '</div>';
+
   $result .= '</div>';
   $result .= '</article>';
 
--- linkedevents/events.php
+++ linkedevents/events.php
@@ -1,5 +1,31 @@
 <?php
 
+  /**
+   * Returns localized value with given language
+   * 
+   * @param array values
+   * @return string value or empty string if not defined
+   */
+  function getLocalizedValue($values, $language) {
+    $result = $values[$language];
+    return $result ? $result : "";
+  }
+
+  /**
+   * Formats location address 
+   * 
+   * @param \Metatavu\LinkedEvents\Model\Place $location location
+   * @param string $language lanuage
+   * @return string formated location address
+   */
+  function formatLocationAddress($location, $language) {
+    $name = getLocalizedValue($location["name"], $language);
+    $streetAddress = getLocalizedValue($location["streetAddress"], $language);
+    $postalCode = $location["postalCode"];
+    $locality = getLocalizedValue($location["addressLocality"], $language);
+    return sprintf('<p>%s, %s %s %s</p>', $name, $streetAddress, $postalCode, $locality);
+  }
+
   foreach ($data->events as $event) {
     include __DIR__ . "/event.php";
   }
