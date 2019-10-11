e25f87ba029f8fced6fa2c380dc3219bd69cb0be
Antti Leppä <antti.leppa@metatavu.fi>
2019-10-11 14:22:32 +0300

    Fixed issue with duplicate functions

--- linkedevents/events.php
+++ linkedevents/events.php
@@ -1,29 +1,37 @@
 <?php
 
-  /**
-   * Returns localized value with given language
-   * 
-   * @param array values
-   * @return string value or empty string if not defined
-   */
-  function getLocalizedValue($values, $language) {
-    $result = $values[$language];
-    return $result ? $result : "";
+  if (!function_exists("getLocalizedValue")) {
+
+    /**
+     * Returns localized value with given language
+     * 
+     * @param array values
+     * @return string value or empty string if not defined
+     */
+    function getLocalizedValue($values, $language) {
+      $result = $values[$language];
+      return $result ? $result : "";
+    }
+    
   }
 
-  /**
-   * Formats location address 
-   * 
-   * @param \Metatavu\LinkedEvents\Model\Place $location location
-   * @param string $language lanuage
-   * @return string formated location address
-   */
-  function formatLocationAddress($location, $language) {
-    $name = getLocalizedValue($location["name"], $language);
-    $streetAddress = getLocalizedValue($location["streetAddress"], $language);
-    $postalCode = $location["postalCode"];
-    $locality = getLocalizedValue($location["addressLocality"], $language);
-    return sprintf('<p>%s, %s %s %s</p>', $name, $streetAddress, $postalCode, $locality);
+  if (!function_exists("formatLocationAddress")) {
+
+    /**
+     * Formats location address 
+     * 
+     * @param \Metatavu\LinkedEvents\Model\Place $location location
+     * @param string $language lanuage
+     * @return string formated location address
+     */
+    function formatLocationAddress($location, $language) {
+      $name = getLocalizedValue($location["name"], $language);
+      $streetAddress = getLocalizedValue($location["streetAddress"], $language);
+      $postalCode = $location["postalCode"];
+      $locality = getLocalizedValue($location["addressLocality"], $language);
+      return sprintf('<p>%s, %s %s %s</p>', $name, $streetAddress, $postalCode, $locality);
+    }
+
   }
 
   foreach ($data->events as $event) {
